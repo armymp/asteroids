f5f147e3b62a675abb1573b88a169e7e45f0b72a
armymp <67810944+armymp@users.noreply.github.com>
2024-11-14 10:45:59 -0500

    add player rendering and rotation to game

--- main.py
+++ main.py
@@ -3,20 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
 
 
 if __name__ == "__main__":
