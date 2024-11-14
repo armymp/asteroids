ded6786a187b77f51ef069e4740cbfd18f271ad8
armymp <67810944+armymp@users.noreply.github.com>
2024-11-14 13:30:46 -0500

    add containers to organize game objects

--- main.py
+++ main.py
@@ -9,21 +9,31 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     dt = 0
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        dt = clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
-        
 
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
